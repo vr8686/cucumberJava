53d1d3467ef4792688fac679a6237e862984b020
vr8686 <rublev.vv@gmail.com>
2023-09-22 21:04:04 -0400

    Fixed AssertString issue

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/my-report.html", "json:target/json-report.json"},
+        //plugin = {"html:target/my-report.html", "json:target/json-report.json"},
+        plugin = {"html:target/my-report.html", "json:target/reports.json"},
         publish = false,
         features = {"src/test/resources/features/OpenCircle/"},
         glue = {"/hooks", "/steps", "/support"}
